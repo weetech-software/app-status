c18f581aa22d0a53760fa92534b9bb055159e01b
Jason Wee <peichieh@gmail.com>
2025-10-04 09:41:42 +0800

    added context

--- frontend/src/service/FirewallService.js
+++ frontend/src/service/FirewallService.js
@@ -2,3 +2,9 @@ import React from "react";
 import Contextualizer from "../context/Contextualizer";
 import ProvidedServices from "../context/ProvidedServices";
 
+const FirewallServiceContext = Contextualizer.createContext(
+   ProvidedServices.FirewallService
+);
+export const useFirewallService = () =>
+   Contextualizer.useContext(ProvidedServices.FirewallService);
+
