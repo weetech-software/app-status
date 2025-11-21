39a6ab0911100d0521670bc7dd481cb48b820eaf
Jason Wee <peichieh@gmail.com>
2025-11-22 03:11:51 +0800

    added method syn_recv_add

--- frontend/src/service/FirewallService.js
+++ frontend/src/service/FirewallService.js
@@ -234,5 +234,38 @@ const FirewallService = ({ children }) => {
          }
      },
 
+     // post sync-recv/add   /api/firewall/syn-recv/add
+     async syn_recv_add(content) {
+         const response = await fetch(
+         `${process.env.REACT_APP_API_URL}/api/firewall/syn-recv/add`,
+           {
+             method: "POST",
+             headers: {
+               "x-access-token": localStorage.getItem("bearerToken"),
+               "content-type": "application/json",
+             },
+             body: JSON.stringify(content),
+           }
+         );
+         if (response.ok) {
+           return new Promise((resolve) => {
+                resolve(true);
+           });
+         } else {
+           response.json().then((json) => {
+              if (json.message === "Token is expired") {
+                  const navigateState = {
+                      state: { message: "session expired" },
+                  };
+                  return new Promise((_, reject) => reject(navigateState));
+              }
+           });
+           const navigateState = {
+              state: { message: "unauth" },
+           };
+           return new Promise((_, reject) => reject(navigateState));
+         }
+     },
+
   }
 }
