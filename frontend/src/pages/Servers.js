98ecbd4315a8c98b1586488589bc77a5aefe6675
Jason Wee <peichieh@gmail.com>
2026-01-24 02:44:00 +0800

    initial servers

---
+++ frontend/src/pages/Servers.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import BasicModal from "../components/Modal";
+
