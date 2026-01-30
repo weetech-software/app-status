aa73ba483eb5ddb1f4f6c1d1ba6a9f2db6493cf3
Jason Wee <peichieh@gmail.com>
2026-01-31 07:47:53 +0800

    added initialState

--- frontend/src/pages/Servers.js
+++ frontend/src/pages/Servers.js
@@ -7,4 +7,20 @@ import BottomNavigation from "@mui/material/BottomNavigation";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import BasicModal from "../components/Modal";
+import Breadcrumb from "../components/Breadcrumb";
+import { ChartLine } from "../components/dashboard_internal/Charts";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { useUserDispatch, signOut } from "../context/UserContext";
+import { routes } from "../core/config";
 
+const initialState = {
+   period: "hourly",
+   filter: "all",
+   monScripts: [
+      {
+         monscript: "",
+         description: "",
+      },
+   ],
+   servers: [],
+};
