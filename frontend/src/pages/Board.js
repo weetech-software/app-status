a7252a832aecea07f8cc0295e0073ec2467e4c2b
Jason Wee <peichieh@gmail.com>
2026-08-15 10:20:14 +0800

    import require component

---
+++ frontend/src/pages/Board.js
@@ -0,0 +1,6 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+
+import BoardComponent from "../components/Board";
+
