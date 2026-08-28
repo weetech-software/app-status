780a46cf8282d9a6ea4e538836841c91c661553d
Jason Wee <peichieh@gmail.com>
2026-08-29 01:20:10 +0800

    complete board implementation

--- frontend/src/pages/Board.js
+++ frontend/src/pages/Board.js
@@ -6,7 +6,18 @@ import BoardComponent from "../components/Board";
 
 const Board = () => {
    return (
-
+     <Paper elevation={6} sx={{m:2, p:1}}>
+         <Grid container alignItems="center" justifyContent="center">
+            <Grid item xs={12}>
+               <Typography variant="h3" gutterBottom component="div">
+                  Board
+               </Typography>
+            </Grid>
+            <Grid item xs={12}>
+               <BoardComponent />
+            </Grid>
+         </Grid>
+     </Paper>
    );
 };
 
